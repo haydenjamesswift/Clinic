3c3ba5b32b5fd6a3fbe62114c4b2de1c4c22b0ae
haydenjamesswift <38249174+haydenjamesswift@users.noreply.github.com>
2018-04-26 15:01:45 +1000

    Update Pet.java

--- Pet.java
+++ Pet.java
@@ -14,35 +14,9 @@ public class Pet {
 	private String no = "no";
 	
 	// Set Pet Type
-	public void setType()
+	public void setType(String type)
 	{
-		Scanner keyboard = new Scanner(System.in);
-		int check = 0;
-		
-		while (check == 0)
-		{
-			System.out.println("Is your pet a cat or a dog?");
-			type = keyboard.nextLine();
-			
-			
-			if (type.equalsIgnoreCase("dog"))
-			{
-				this.type = "Dog";
-				check = 1;
-			}
-			else if (type.equalsIgnoreCase("cat"))
-			{
-				this.type = "Cat";
-				check = 1;
-			}
-			else
-			{
-				System.out.println("Invalid pet type. Please try again. ");
-				System.out.println("------------------------------------------------------------");
-				check = 0;
-			}
-		}
-		
+		this.type = type;
 	}
 	
 	// Get Pet Type
@@ -52,38 +26,9 @@ public class Pet {
 	}
 	
 	// Set Pet Size
-	public void setSize()
+	public void setSize(String size)
 	{
-		Scanner keyboard = new Scanner(System.in);
-		int check = 0;
-		
-		while (check == 0)
-		{
-			System.out.println("What size is your pet? Small, medium or large?");
-			size = keyboard.nextLine();
-			
-			if (size.equalsIgnoreCase("small"))
-			{
-				this.size = "Small";
-				check = 1;
-			}
-			else if (size.equalsIgnoreCase("medium"))
-			{
-				this.size = "Medium";
-				check = 1;
-			}
-			else if(size.equalsIgnoreCase("large"))
-			{
-				this.size = "Large";
-				check = 1;
-			}
-			else
-			{
-				System.out.println("Invalid pet size. Please try again.");
-				System.out.println("------------------------------------------------------------");
-				check = 0;
-			}
-		}
+		this.size = size;
 	}
 	
 	//Get Pet Size
@@ -111,28 +56,9 @@ public class Pet {
 	}
 	
 	//Set Pet Weight
-	public void setWeight()
+	public void setWeight(Double weight)
 	{
-		Scanner keyboard = new Scanner (System.in);
-		int check = 0;
-		
-		while (check == 0)
-		{
-			System.out.println("What is your pet's weight in kilograms?");
-			this.weight = keyboard.nextDouble();
-			
-			if (weight>0)
-			{
-				this.weight = weight;
-				check = 1;
-			}
-			else
-			{
-				System.out.println("Error. Please input a positive number.");
-				System.out.println("------------------------------------------------------------");				
-				check = 0;
-			}
-		}
+		this.weight = weight;
 	}
 	
 	//Get Pet Weight
@@ -142,28 +68,9 @@ public class Pet {
 	}
 	
 	//Set Pet Age
-	public void setAge()
+	public void setAge(int age)
 	{
-		Scanner keyboard = new Scanner(System.in);
-		int check = 0;
-		
-		while (check == 0)
-		{
-			System.out.println("How old is " +this.name +"?");
-			this.age = keyboard.nextInt();
-			
-			if (age>0)
-			{
-				this.age = age;
-				check = 1;
-			}
-			else
-			{
-				System.out.println("Error. Please input a positive number.");
-				System.out.println("------------------------------------------------------------");				
-				check = 0;
-			}
-		}
+		this.age = age;
 	}
 	
 	//Get Pet Age
@@ -190,7 +97,7 @@ public class Pet {
 	{
 		this.setType();
 		this.setName(name);
-		this.setSize();
+		this.setSize(size);
 		this.setWeight();
 		this.setAge();
 		this.setPetDoctor("No Doctor Assigned");
@@ -204,7 +111,7 @@ public class Pet {
 		this.size = null;
 		this.weight = 0;
 		this.age = 0;
-		this.petDoctor = null;
+		this.petDoctor = "No Doctor Assigned";
 	}
 	
 	//Analyse Dog
@@ -215,15 +122,15 @@ public class Pet {
 		System.out.println("Size: "+this.getSize());
 		System.out.println("Weight: "+this.getWeight());
 		
-		if ((this.getSize().equalsIgnoreCase("small")) && (this.getWeight()>6)&& (this.getType().equalsIgnoreCase("Dog")))
+		if ((this.getSize().equalsIgnoreCase("small")) && (this.getWeight()>6))
 		{
 			System.out.println(this.getName()+" is overweight.");
 		}
-		else if ((this.getSize().equalsIgnoreCase("medium")) && (this.getWeight()>9)&& (this.getType().equalsIgnoreCase("Dog")))
+		else if ((this.getSize().equalsIgnoreCase("medium")) && (this.getWeight()>9))
 		{
 			System.out.println(this.getName()+" is overweight.");
 		}
-		else if ((this.getSize().equalsIgnoreCase("large")) && (this.getWeight()>12)&& (this.getType().equalsIgnoreCase("Dog")))
+		else if ((this.getSize().equalsIgnoreCase("large")) && (this.getWeight()>12))
 		{
 			System.out.println(this.getName()+" is overweight.");
 		}
@@ -241,15 +148,15 @@ public class Pet {
 			System.out.println("Size: "+this.getSize());
 			System.out.println("Weight: "+this.getWeight());
 			
-			if ((this.getSize().equalsIgnoreCase("small")) && (this.getWeight()>4)&& (this.getType().equalsIgnoreCase("Cat")))
+			if ((this.getSize().equalsIgnoreCase("small")) && (this.getWeight()>4))
 			{
 				System.out.println(this.getName()+" is overweight.");
 			}
-			else if ((this.getSize().equalsIgnoreCase("medium")) && (this.getWeight()>6)&& (this.getType().equalsIgnoreCase("Cat")))
+			else if ((this.getSize().equalsIgnoreCase("medium")) && (this.getWeight()>6))
 			{
 				System.out.println(this.getName()+" is overweight.");
 			}
-			else if ((this.getSize().equalsIgnoreCase("large")) && (this.getWeight()>8)&& (this.getType().equalsIgnoreCase("Cat")))
+			else if ((this.getSize().equalsIgnoreCase("large")) && (this.getWeight()>8))
 			{
 				System.out.println(this.getName()+" is overweight.");
 			}
@@ -268,9 +175,6 @@ public class Pet {
 		System.out.println("Weight: " +this.getWeight() + " kilograms.");
 		System.out.println("Age: "+this.getAge()+" years.");
 		System.out.println("Doctor: "+this.getPetDoctor());
-
-
-
 	}
 }
 
